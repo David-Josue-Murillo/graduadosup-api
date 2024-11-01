19d951a30f0fa7034f14f49e8b1c2765c3230ef3
David Murillo <david.josue.murillo23@gmail.com>
2024-10-31 22:22:23 -0500

    feat: :necktie: Protecting data that may or not may not be assigned

--- app/Models/Faculty.php
+++ app/Models/Faculty.php
@@ -10,6 +10,23 @@ class Faculty extends Model
     //
     use HasFactory;
 
+    /**
+     * Atributos que pueden ser asignados
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Ocultando los datos que no se deben mostrar
+     * @var array 
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
     public function careers() {
         return $this->hasMany(Career::class);
     }
