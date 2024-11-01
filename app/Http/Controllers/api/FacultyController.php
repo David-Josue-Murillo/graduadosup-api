31a90827099b233b83b17094bc975ebad2b0b5d3
David Murillo <david.josue.murillo23@gmail.com>
2024-10-31 23:05:52 -0500

    feat: :necktie: Adding function to create new faculty

--- app/Http/Controllers/api/FacultyController.php
+++ app/Http/Controllers/api/FacultyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Faculty;
 use Illuminate\Http\Request;
+use Validator;
 
 class FacultyController extends Controller
 {
@@ -34,19 +35,48 @@ class FacultyController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
         //
+        $validator =  Validator::make($request->all(), [
+            'name' => 'required|max:100'
+        ]);
+
+        // Validando datos
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error en la validación de los datos',
+                'errors'=> $validator->errors(),
+                'status' => 400
+            ];
+
+            return response()->json($data, 400);
+        }
+        
+        $faculty = Faculty::create([
+            'name' => $request->name
+        ]);
+
+        // Si no se pudo crear la facultad
+        if(!$faculty){
+            $data = [
+                'message' => 'Error al crear la facultad',
+                'status' => 400
+            ];
+
+            return response()->json($data, 400);
+        }
+
+        // Datos de respuesta
+        $data = [
+            'message' => 'Nueva facultad creada',
+            'facultad' => $faculty,
+            'status' => 201
+        ];
+
+        return response()->json($data, 201);
     }
 
     /**
