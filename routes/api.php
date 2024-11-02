98bd3988644783a6a5e211a0b901884c3cf8b3be
David Murillo <david.josue.murillo23@gmail.com>
2024-11-02 17:41:11 -0500

    feat: :sparkles: Establishing routes to interact with tha careers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\api\FacultyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\CareerController;
+use App\Http\Controllers\api\FacultyController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,12 @@ Route::prefix('faculties')->group(function () {
     Route::put('/{faculty}', [FacultyController::class, 'update']); // PUT /faculties/{id} - Actualiza una facultad específica
     Route::delete('/{faculty}', [FacultyController::class, 'destroy']); // DELETE /faculties/{id} - Elimina una facultad específica
 });
+
+Route::prefix('careers')->group(function () {
+    Route::get('/', [CareerController::class, 'index']);           // GET /career - Lista todas las carreras
+    Route::post('/', [CareerController::class, 'store']);          // POST /career - Crea una nueva carrera
+    Route::get('/{career}', [CareerController::class, 'show']);   // GET /career/{id} - Muestra una carrera específica
+    Route::put('/{career}', [CareerController::class, 'update']); // PUT /career/{id} - Actualiza una carrera específica
+    Route::delete('/{career}', [CareerController::class, 'destroy']); // DELETE /career/{id} - Elimina una carrera específica
+});
+
