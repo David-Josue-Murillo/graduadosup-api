95cfe99280de43bb1a449669895d87e13c6fb0cd
David Murillo <david.josue.murillo23@gmail.com>
2024-11-03 16:37:44 -0500

    feat: :card_file_box: Specifying data that can be displayed

--- app/Models/Campu.php
+++ app/Models/Campu.php
@@ -6,7 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Campu extends Model
 {
-    //
+    /**
+     * Attributes that can be assigned
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * Hiding data that should not be displayed
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
     public function graduates() {
         return $this->hasMany(Graduate::class);
     }
