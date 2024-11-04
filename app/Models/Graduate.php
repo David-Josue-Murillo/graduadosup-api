ef6e91c77ba8db8936877fafaaab79770a24e197
David Murillo <david.josue.murillo23@gmail.com>
2024-11-04 15:38:55 -0500

    feat: :passport_control: Establishing protection in the attributes

--- app/Models/Graduate.php
+++ app/Models/Graduate.php
@@ -6,6 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Graduate extends Model
 {
+
+    /**
+     * butes that can be assigned
+     * @var array
+     */
+    protected $fillable = [
+        'quantity',
+        'year',
+        'campus_id',
+        'career_id'
+    ];
+
+
+    /**
+     * Hidding data that should not be displayed
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
     public function campu() {
         return $this->belongsTo(Campu::class);
     }
