6c13e4a848be5954c5ea56e486744cd5e3ab650c
David Murillo <david.josue.murillo23@gmail.com>
2024-11-04 15:51:45 -0500

    feat: :necktie: CRUD operations created

--- app/Http/Controllers/api/GraduateController.php
+++ app/Http/Controllers/api/GraduateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\NumGraduatesRequest;
 use App\Models\Graduate;
 use Illuminate\Http\Request;
 
@@ -13,54 +14,57 @@ class GraduateController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $num_graduates = Graduate::all();
+        return $num_graduates->isEmpty()
+        ? $this->jsonResponse('No hay datos', [], 200)
+        : $this->jsonResponse('Datos obtenidos exitosamente', $num_graduates, 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(NumGraduatesRequest $request)
     {
-        //
+        $graduates = Graduate::create([
+            'quantity' => $request->quantity,
+            'year' => $request->year,
+            'campus_id' => $request->campus_id,
+            'career_id' => $request->career_id
+        ]);
+
+        return $this->jsonResponse("Datos guardados exitosamente", $graduates, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Graduate $graduate)
+    public function show(int $graduate_id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Graduate $graduate)
-    {
-        //
+        $graduate = Graduate::findOrFail($graduate_id);
+        return $this->jsonResponse('Datos obtenido exitosamente', $graduate, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Graduate $graduate)
+    public function update(NumGraduatesRequest $request, Graduate $graduate)
     {
-        //
+        $graduate->update([
+            'quantity' => $request->quantity,
+            'year' => $request->year,
+            'campus_id' => $request->campus_id,
+            'career_id' => $request->career_id
+        ]);
+
+        return $this->jsonResponse('Dato actualizado exitosamente', $graduate, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Graduate $graduate)
+    public function destroy(NumGraduatesRequest $graduate)
     {
-        //
+        $graduate->delete();
+        return $this->jsonResponse('Dato eliminado exitosamente', $graduate, 200);
     }
 }
