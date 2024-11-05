edd11f54d7d9d98a152a33b7e7200076595d3ebc
David Murillo <david.josue.murillo23@gmail.com>
2024-11-04 20:29:05 -0500

    feat: :white_check_mark: Adding fake data to the db using faker

--- app/Models/Campu.php
+++ app/Models/Campu.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Campu extends Model
 {
+    /** @use HasFactory<\Database\Factories\CampuFactory> */
+    use HasFactory, Notifiable;
+
     /**
      * Attributes that can be assigned
      * @var array
