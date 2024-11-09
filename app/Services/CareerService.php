28fd1bbb7c59a517a6053ca65040bafe73031637
David Murillo <david.josue.murillo23@gmail.com>
2024-11-08 20:09:44 -0500

    feat: :sparkles: Implementing services for careers

---
+++ app/Services/CareerService.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Services;
+
+use Illuminate\Database\Eloquent\Builder;
+use Request;
+
+class CareerService {
+
+    public function verifyFilter(Builder $query, Request $request):Builder {
+        if($request->filled('name')){
+            $query->where('name', $request->name);
+        }
+
+        if($request->filled('faculty_id')){
+            $query->where('faculty_id', $request->faculty_id);
+        }
+
+        return $query;
+    }
+}
