94460751359fd8101e516af66dca1decfc6d2ec2
David Murillo <david.josue.murillo23@gmail.com>
2024-11-10 16:40:29 -0500

    feat: :sparkles: Create CRUD method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,32 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    protected $services;
+
+    public function __construct(UserService $services)
+    {
+        $this->services = $services;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $users = $this->services->formatAllData(User::all());
+        return $this->jsonResponse('ok', $users, 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        $newUser = User::create($request->validated());
+        return $this->jsonResponse('Usuario creado exitosamente', $newUser, 201);
     }
 
     /**
@@ -35,7 +40,7 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        
     }
 
     /**
