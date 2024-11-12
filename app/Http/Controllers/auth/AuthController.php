cd600c907f365a7d46fe15dda6b0bb3021a838b3
David Murillo <david.josue.murillo23@gmail.com>
2024-11-11 20:33:25 -0500

    feat: :sparkles: Implemenytation login method

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Requests\auth\LoginRequest;
 use App\Services\AuthService;
 
 class AuthController
@@ -11,5 +12,31 @@ class AuthController
     public function __construct(AuthService $services)
     {
         $this->services = $services;
+    }
+
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            //'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
     }
 }
