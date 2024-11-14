0d8da9fcb8cc4c5d92a7df436e1b80677ac342f8
David Murillo <david.josue.murillo23@gmail.com>
2024-11-14 15:24:43 -0500

    feat: :sparkles: Send email settings

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Mail\TestMarkdown;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Emails
+Route::get('/email', function () {
+    Mail::to('dm514821@gmail.com')->send(new TestMarkdown());
 });
