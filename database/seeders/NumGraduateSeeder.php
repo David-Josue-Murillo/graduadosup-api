b3a93d15b23d0417cf458d59824b4c23d0c4fad6
David Murillo <david.josue.murillo23@gmail.com>
2024-11-18 18:01:27 -0500

    test: :test_tube: Created seeders to test

---
+++ database/seeders/NumGraduateSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Campu;
+use App\Models\Career;
+use App\Models\Faculty;
+use App\Models\NumGraduate;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class NumGraduateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class,
+            CampuSeeder::class,
+            FacultySeeder::class,
+            CareerSeeder::class,
+        ]);
+
+        NumGraduate::factory()->create([
+            'quantity' => '100',
+            'year' => '2021',
+            'campus_id' => 1,
+            'career_id' => 1
+        ]);
+
+        NumGraduate::factory()->create([
+            'quantity' => '25',
+            'year' => '2022',
+            'campus_id' => 2,
+            'career_id' => 2
+        ]);
+
+        NumGraduate::factory()->create([
+            'quantity' => '70',
+            'year' => '2023',
+            'campus_id' => 2,
+            'career_id' => 3
+        ]);
+
+        NumGraduate::factory()->create([
+            'quantity' => '15',
+            'year' => '2018',
+            'campus_id' => 1,
+            'career_id' => 4
+        ]);
+
+        NumGraduate::factory()->create([
+            'quantity' => '140',
+            'year' => '2019',
+            'campus_id' => 1,
+            'career_id' => 5
+        ]);
+    }
+}
--- tests/Feature/NumGraduate/DisplayRecordOfNumGraduateTest.php
+++ tests/Feature/NumGraduate/DisplayRecordOfNumGraduateTest.php
@@ -2,12 +2,9 @@
 
 namespace Tests\Feature\NumGraduateTest;
 
+use Database\Seeders\NumGraduateSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
-use Database\Seeders\CampuSeeder;
-use Database\Seeders\CareerSeeder;
-use Database\Seeders\FacultySeeder;
-use Database\Seeders\UserSeeder;
 use Tests\TestCase;
 
 class DisplayRecordOfNumGraduateTest extends TestCase
@@ -38,21 +35,8 @@ class DisplayRecordOfNumGraduateTest extends TestCase
     {
         parent::setUp();
         $this->seed([
-            UserSeeder::class,
-            FacultySeeder::class,
-            CampuSeeder::class,
-            CareerSeeder::class
+            NumGraduateSeeder::class
         ]);
-    }
-
-    protected function validGraduateData(array $overrides = []): array
-    {
-        return array_merge([
-            'quantity' => 100,
-            'year' => now()->year,
-            'campus_id' => 1,
-            'career_id' => 1,
-        ], $overrides);
     }
 
     /** @test */
@@ -70,27 +54,11 @@ class DisplayRecordOfNumGraduateTest extends TestCase
     }
 
     /** @test */
-    public function register_a_new_number_of_graduates(): void
-    {
-        $data = $this->validGraduateData();
-
-        $response = $this->apiAs(User::find(1), 'post', self::URL, $data);
-
-        $response->assertStatus(201);
-        $response->assertJsonStructure([
-            'message',
-            'data',
-            'status',
-            'errors'
-        ]);
-    }
-
-    /** @test */
     public function it_should_returned_all_graduates_that_matching_with_campus_id(): void
     {
-        $this->register_a_new_number_of_graduates();
-        $response = $this->apiAs(User::find(1), 'get', '/api'.self::URL.'?campus_id=1');
+        $response = $this->apiAs(User::find(1), 'get', self::URL.'?campus_id=1');
 
+        dd($response);
         $response->assertStatus(200)
             ->assertJsonStructure(self::JSON_RESPONSE)
             ->assertJsonFragment([
@@ -104,8 +72,7 @@ class DisplayRecordOfNumGraduateTest extends TestCase
     /** @test */
     public function it_should_returned_all_graduates_that_matching_with_career_id(): void
     {
-        $this->register_a_new_number_of_graduates();
-        $response = $this->apiAs(User::find(1), 'get', '/api'.self::URL.'?career=1');
+        $response = $this->apiAs(User::find(1), 'get', self::URL.'?career=1');
 
         $response->assertStatus(200)
             ->assertJsonStructure(self::JSON_RESPONSE)
@@ -124,8 +91,7 @@ class DisplayRecordOfNumGraduateTest extends TestCase
     /** @test */
     public function it_should_returned_all_graduates_that_matching_with_the_year(): void
     {
-        $this->register_a_new_number_of_graduates();
-        $response = $this->apiAs(User::find(1), 'get', '/api'.self::URL.'?year=2024');
+        $response = $this->apiAs(User::find(1), 'get', self::URL.'?year=2024');
 
         $response->assertStatus(200)
             ->assertJsonStructure(self::JSON_RESPONSE)
