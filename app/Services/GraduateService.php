c7ca000d5da8d4c4206ad7bb934b7144bb985cac
David Murillo <david.josue.murillo23@gmail.com>
2024-11-26 22:02:36 -0500

    :zap: Adding resources in the numgraduates response

--- app/Services/GraduateService.php
+++ app/Services/GraduateService.php
@@ -81,44 +81,4 @@ class GraduateService
             'career_id' => $request->career_id
         ]);
     }
-
-    /**
-     * Formats and returns custom data structure for NumGraduates with associated details.
-     *
-     * @param NumGraduate $numGraduates
-     * @return array
-     */
-    public function formatGraduatedData(NumGraduate $graduate): array
-    {
-        return [
-            'id' => $graduate->id,
-            'quantity' => $graduate->quantity,
-            'year' => $graduate->year,
-            'campus' => [
-                'id' => $graduate->campus->id,
-                'name' => $graduate->campus->name,
-            ],
-            'career' => [
-                'id' => $graduate->career->id,
-                'name' => $graduate->career->name,
-                'faculty' => [
-                    'id' => $graduate->career->faculty->id,
-                    'name' => $graduate->career->faculty->name,
-                ]
-            ]
-        ];
-    }
-
-    /**
-     * Format all data for the number of graduates
-     *
-     * @param NumGraduate $graduate
-     * @return array Datos formateados para la respuesta
-     */
-    public function formatNumGraduatedData($numGraduates)
-    {
-        return $numGraduates->map(function ($graduate) {
-            return $this->formatGraduatedData($graduate);
-        });
-    }
 }
