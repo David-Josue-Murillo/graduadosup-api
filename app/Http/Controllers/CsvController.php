6b8283e4cbaf1aca36f465ed1cf81b78aca725a9
David Murillo <david.josue.murillo23@gmail.com>
2024-12-27 11:41:25 -0500

    :memo: Documenting code

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -12,6 +12,15 @@ class CsvController extends Controller
 {
     private const CSV_PATH = 'app/csv/';
 
+    /**
+     * Processes multiple CSV files and maps their data to corresponding models.
+     *
+     * This method processes three CSV files: 'faculties.csv', 'campus.csv', and 'careers.csv'.
+     * It maps the data from these files to the Faculty, Campus, and Career models respectively.
+     * The column mappings for each file are specified in the method calls.
+     *
+     * @return void
+     */
     public function processCsv(): void
     {
         $this->processCsvData('faculties.csv', Faculty::class, ['name' => 'faculty']);
@@ -22,6 +31,19 @@ class CsvController extends Controller
         ]);
     }
 
+    /**
+     * Processes a CSV file and maps its data to a specified model.
+     *
+     * This method reads a CSV file, verifies its existence, and processes its records.
+     * It maps the CSV columns to the model's database columns as specified in the column mappings.
+     * The data is then inserted into the database within a transaction.
+     *
+     * @param string $filename The name of the CSV file to process.
+     * @param string $model The model class to map the data to.
+     * @param array $columnMappings An associative array mapping CSV columns to database columns.
+     *
+     * @return void
+     */
     private function processCsvData(string $filename, string $model, array $columnMappings): void
     {
         $filePath = storage_path(self::CSV_PATH . $filename);
@@ -42,6 +64,15 @@ class CsvController extends Controller
         });
     }
 
+    /**
+     * Verifies if a file exists at the given path.
+     *
+     * This method checks if a file exists at the specified path.
+     * If the file does not exist, it returns a JSON response with an error message and a 404 status code.
+     *
+     * @param string $file The path to the file to verify.
+     * @return bool Returns true if the file exists, false otherwise.
+     */
     private function verifyIfExistFile(string $file): bool
     {
         if (file_exists($file)) return true;
@@ -49,6 +80,16 @@ class CsvController extends Controller
         return false;
     }
 
+    /**
+     * Reads a CSV file and returns its records as an iterator.
+     *
+     * This method creates a CSV reader from the specified file path,
+     * sets the header offset to 0 to use the first row as the header,
+     * and returns an iterator for the CSV records.
+     *
+     * @param string $path The path to the CSV file.
+     * @return \Iterator An iterator for the CSV records.
+     */
     private function readCsv(string $path): \Iterator
     {
         $csv = Reader::createFromPath($path, 'r');
